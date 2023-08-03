e73604ef54ea0a9bd461807f31df2a8c6e7a2e67
Rodrigo Cerqueira <rodrigocerqueira@outlook.com>
2023-08-02 21:30:04 -0300

    Twilio SMS improvements

--- backend/src/main/java/com/myapp/dsmeta/controllers/SaleController.java
+++ backend/src/main/java/com/myapp/dsmeta/controllers/SaleController.java
@@ -5,6 +5,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,8 +33,8 @@ public class SaleController {
 		return ResponseEntity.ok().body(list);
 	}
 	
-	@GetMapping("/notification")
-	public void notifySms() {
-		smsService.sendSms();
+	@GetMapping("/{id}/notification")
+	public void notifySms(@PathVariable Long id) {
+		smsService.sendSms(id);
 	}
 }
